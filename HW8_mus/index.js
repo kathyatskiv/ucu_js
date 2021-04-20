309edbed89802a1c94a687f24acbdbd01d8585f7
Yatskiv Kath <iamkate4@gmail.com>
2021-04-20 23:31:14 +0300

    departments' objects get + objDate

--- HW8_mus/index.js
+++ HW8_mus/index.js
@@ -27,17 +27,11 @@ async function departmentBtn(element){
 
     let id = element.id[0];
 
-    const request  = await fetch(` https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=${id}`);
+    const request  = await fetchById(id);
     const objects  = (await request.json())["objectIDs"];
 
     const top10obj =objects.slice(0, 10);
     console.log(top10obj);
-
-    // const r  = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${top10obj[0]}`);
-    // const o  = (await r.json());
-
-    // console.log(o);
-
 
     const requests = top10obj.map(objId => fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objId}`));
 
@@ -48,20 +42,24 @@ async function departmentBtn(element){
     .then(responses => {
 
         for(let i = 0; i < responses.length; i++) {
-            getInfo(responses[i], i);
+            getInfo(responses[i], i, id);
           }
     })
 }
 
-async function getInfo(promise, i){
+const fetchById = (id) => fetch(` https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=${id}`)
+
+async function getInfo(promise, i, id){
     const container = document.getElementById("container");
 
     const row = (i<5) ? document.getElementById("row0") : document.getElementById("row1");
 
     let ans = await promise.json();
+
     row.innerHTML += `<div class="col-sm elem">
     <img src="${ans["primaryImage"]}" alt="${ans["title"]}" class="elem-image">
-    ${ans["title"]}
+    <p class="elem-title">${ans["title"]}</p>
+    <p>${ans["objectDate"]}</p>
     </div>`
 
     console.log(ans);
